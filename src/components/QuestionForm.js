73d44660a736cfbcb91aae285ef99a42491f86b0
Milhan Osman <milhanomarosman@gmail.com>
2023-04-24 17:24:18 +0300

    completed

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+// import QuestionList from "./QuestionList";
 
-function QuestionForm(props) {
+function QuestionForm({ addQuestion }) {
+  
   const [formData, setFormData] = useState({
     prompt: "",
     answer1: "",
@@ -19,7 +21,26 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: formData.prompt,
+        answers: [
+          formData.answer1,
+          formData.answer2,
+          formData.answer3,
+          formData.answer4,
+        ],
+        correctIndex: formData.correctIndex,
+      }),
+    })
+      .then((r) => r.json())
+      .then((data) => {
+        addQuestion(data);
+      });
   }
 
   return (
